a3f794402d79ed6c79af3f26cda9e81378dd2687
Davidama <davidarteromartinez@gmail.com>
2021-09-06 19:22:35 +0200

    crud finished

--- routes.js
+++ routes.js
@@ -23,17 +23,51 @@ routes.post('/', (req, res)=>{
        if(err){
             return res.status(400).send(err);
         }
-        console.log(req.body)
-        
-        // else{
-        //     conn.query('INSERT INTO books SET ?', [req.body], (err, rows)=>{
+                
+        else{
+            conn.query('INSERT INTO books SET ?', [req.body], (err, rows)=>{
                
-        //         if(err) return res.status(400).send(err);
+                if(err) return res.status(400).send(err);
 
-        //         return res.status(200).json(rows);
-        //     });
-        // }
+                return res.status(200).send("book inserted correctly");
+            });
+        }
+    })
+});
 
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn) =>{
+     
+       if(err){
+            return res.status(400).send(err);
+        }
+                
+        else{
+            conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows)=>{
+               
+                if(err) return res.status(400).send(err);
+
+                return res.status(200).send("book removed succesfully");
+            });
+        }
+    })
+});
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn) =>{
+     
+       if(err){
+            return res.status(400).send(err);
+        }
+                
+        else{
+            conn.query('UPDATE books SET ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+               
+                if(err) return res.status(400).send(err);
+
+                return res.status(200).send("book updated succesfully");
+            });
+        }
     })
 });
 
